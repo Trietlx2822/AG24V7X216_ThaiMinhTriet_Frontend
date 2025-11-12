b73a76a560982025cbdeb6c37ce1af0d5c0d2bae
Minh Triet <trietag24v7x216@dttx.ctu.edu.vn>
2025-11-12 21:55:19 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -14,21 +14,24 @@ v-if="filteredContactsCount > 0"
 v-model:activeIndex="activeIndex"
 />
 <p v-else>Không có liên hệ nào.</p>
+
 <div class="mt-3 row justify-content-around align-items-center">
 <button class="btn btn-sm btn-primary" @click="refreshList()">
 <i class="fas fa-redo"></i> Làm mới
 </button>
+
 <button class="btn btn-sm btn-success" @click="goToAddContact">
 <i class="fas fa-plus"></i> Thêm mới
 </button>
-<button
-class="btn btn-sm btn-danger"
+
+<button class="btn btn-sm btn-danger" 
 @click="removeAllContacts"
 >
 <i class="fas fa-trash"></i> Xóa tất cả
 </button>
 </div>
 </div>
+
 <div class="mt-3 col-md-6">
 <div v-if="activeContact">
 <h4>
@@ -36,16 +39,28 @@ Chi tiết Liên hệ
 <i class="fas fa-address-card"></i>
 </h4>
 <ContactCard :contact="activeContact" />
+<router-link
+:to="{
+name: 'contact.edit',
+params: { id: activeContact._id },
+}"
+>
+<span class="mt-2 badge badge-warning">
+<i class="fas fa-edit"></i> Hiệu chỉnh</span
+>
+</router-link>
 </div>
 </div>
-
+</div>
 </template>
+
 <script>
 // src/views/ContactBook.vue
 import ContactCard from "@/components/ContactCard.vue";
 import InputSearch from "@/components/InputSearch.vue";
 import ContactList from "@/components/ContactList.vue";
 import ContactService from "@/services/contact.service";
+
 export default {
 components: {
 ContactCard,
@@ -126,4 +141,4 @@ this.refreshList();
 text-align: left;
 max-width: 750px;
 }
-/style>
+</style>
